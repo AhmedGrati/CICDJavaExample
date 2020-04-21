ed79c9e101e7d642cf6ebc6c5039db724c09c2b5
mohamedwaelbenismail <mohamedwaelbenismail@gmail.com>
2020-04-21 21:45:41 +0100

    firstPush

---
+++ src/CICD/MainClass.java
@@ -0,0 +1,11 @@
+package CICD;
+
+public class MainClass {
+    public static void main(String[] args) {
+        int i=0;
+        while(i<10){
+            System.out.println("Hello Jenkins CI/CD : "+i);
+            i++;
+        }
+    }
+}
